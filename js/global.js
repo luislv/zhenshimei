04f861dbf700a7f95f96f595ff685093fd6a1bba
xiyou2014 <lv_chang@hotmail.com>
2017-03-16 18:45:42 +0800

    add:产品详情页

--- js/global.js
+++ js/global.js
@@ -3,11 +3,18 @@ $(function() {
      *  导航下拉列表
      */
     $('.J_shop').on('click', function(e) {
+        var self = $(this);
         e.preventDefault();
         e.stopPropagation();
+        if(self.hasClass('active')) {
+            self.removeClass('active');
+        } else {
+            self.addClass('active');
+        }
         $('.J_shopDropdown').slideToggle('fast');
     });
     $(document).on('click', function() {
+        $('.J_shop').removeClass('active');
         $('.J_shopDropdown').slideUp('fast');
     });
 });
