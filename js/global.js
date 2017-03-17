e453da96f206fcdc90a21464a5e673e20a6285bf
xiyou2014 <lv_chang@hotmail.com>
2017-03-18 01:10:11 +0800

    modify

--- js/global.js
+++ js/global.js
@@ -27,4 +27,33 @@ $(function() {
         // 还原列表项激活状态
         $('.nav li a').eq(index).addClass('active');
     });
+
+    /**
+     * 侧边菜单栏下拉
+     */
+    $('.J_menu').on('click', function() {
+        $(this).siblings('.J_dropDown').slideToggle('fast');
+    });
 });
+
+/**
+ * tab切换
+ */
+function Tab(option){
+    this.root=$(option.root);
+    this.tabTag=this.root.find('#tabon li');//菜单栏
+    this.hidden=this.root.find('.J_tabcon_item');//隐藏内容
+    this.init();
+}
+Tab.prototype={
+    init:function(){
+        var that=this;
+        this.tabTag.each(function(i){
+            $(this).click(function(){
+                that.tabTag.removeClass('active');
+                $(this).addClass('active');
+                that.hidden.eq(i).show().siblings().hide();
+            })
+        })
+    }
+};
