6d260050718a942f44490b1679a16737167ebdca
xiyou2014 <lv_chang@hotmail.com>
2017-03-18 18:18:19 +0800

    modify

--- js/global.js
+++ js/global.js
@@ -39,21 +39,24 @@ $(function() {
 /**
  * tab切换
  */
-function Tab(option){
+function Tab(option, callback){
     this.root=$(option.root);
     this.tabTag=this.root.find('#tabon li');//菜单栏
     this.hidden=this.root.find('.J_tabcon_item');//隐藏内容
-    this.init();
+    this.init(callback);
 }
 Tab.prototype={
-    init:function(){
+    init:function(callback){
         var that=this;
         this.tabTag.each(function(i){
             $(this).click(function(){
                 that.tabTag.removeClass('active');
                 $(this).addClass('active');
                 that.hidden.eq(i).show().siblings().hide();
-            })
-        })
+                if(callback){
+                    callback();
+                }
+            });
+        });
     }
 };
